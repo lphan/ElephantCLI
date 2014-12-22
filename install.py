4420b061123ab34a25b9f5dbca2aee7a2a921747
Long Phan <l.phan@fz-juelich.de>
2014-12-22 16:29:09 +0100

    first commit for ElePhAntCLI, some install, setup & doc-files

---
+++ install.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# import commands
+# import apt
+# import os
+import subprocess
+import pip
+
+# --------------- should be in 1-Script ---------------
+# Script to check on the machine and install the software needed and
+# all necessary dependencies (libs) needed for ElePhAnt
+# Put all packages which will be installed into array
+# packages = ['git', 'python-virtualenv', 'python-pip', 'htop']
+libs = ['pyinstaller', 'numpy', 'scipy', 'quantities', 'neo',
+        'pandas', 'nose', 'sphinx', 'numpydoc', 'task-sdk']
+
+# print "------------------------------------------"
+# if raw_input("Check whether all packages are installed (Y/N)") is 'Y':
+#     for i in range(len(packages)):
+#         devnull = open(os.devnull, "w")
+#         stat = subprocess.call(["dpkg", "-s", packages[i]], stdout=devnull,
+#                                stderr=subprocess.STDOUT)
+#         devnull.close()
+#         if stat != 0:
+#             print "Package "+packages[i]+" not installed."
+#             if raw_input("you want to install it (Y/N)") is 'Y':
+#                 co = "sudo apt-get install "+packages[i]
+#                 print co
+#                 subprocess.call(co, shell=True)
+#             else:
+#                 print "Cancel install", packages[i]
+#
+#         else:
+#             print "Package "+packages[i]+" already installed"
+#             output = 'dpkg -s '+packages[i]+' | grep Version'
+#             # print output
+#             print commands.getoutput(output)
+
+# generate and activate virtualenv
+# workspace = raw_input("Input location of workspace to generate virtual env ")
+# subprocess.call("cd "+workspace, shell=True)
+
+# ---------------- Do it via commands ------------------
+
+# if raw_input("you want to create new virtual environment (Y/N)") is 'Y':
+#     env = raw_input("Input name of virtual environment ")
+#     subprocess.call("virtualenv "+env, shell=True)
+#     print "virtualenv need to be activated via '. "+env+"/bin/activate'"
+#     # path_virlib = env+"/lib/python2.7/site-packages/"
+#     # subprocess.call("ls "+path_virlib, shell=True)
+# else:
+#     pass
+
+# subprocess.check_call(["source ","./virtest/bin/activate"])
+# cmd = "source ./virtest/bin/activate"
+# p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+#                     stderr=subprocess.PIPE)
+# p.wait()
+# subprocess.call("cd ..", shell=True)
+# subprocess.call("source activate", shell=True)
+# venv = ". "+env+"/bin/activate"
+# subprocess.call(venv, shell=True)
+# ------------------------------------------------------
+
+# ---------------- should be in 2-script ---------------
+# Try: setup-tools
+# search all installed packaged using pip
+if raw_input("Check all libraries at current environment (Y/N)") is 'Y':
+    installed_listlib = sorted(["%s==%s" % (i.key, i.version) for i in
+                                pip.get_installed_distributions()])
+    print installed_listlib
+    l = {}
+    for i in range(len(installed_listlib)):
+        key = installed_listlib[i].split('==')[0]
+        value = installed_listlib[i].split('==')[1]
+        # Push lib name and lib version into kind of list of dict
+        l.update({key: value})
+        # print "library name     = ", l[0]
+
+    # Check if necessary library already stay in installed_listlib
+    for i in range(len(libs)):
+        if libs[i] in l:
+            print "lib "+libs[i]+" already installed"
+            print "... its version ", l[libs[i]]
+        else:
+            print "lib "+libs[i]+" not installed"
+            if libs[i] is 'task-sdk':
+                print "Install task-sdk need an access to VPN-EPFL"
+                if raw_input("you want to install it (Y/N)") is 'Y':
+                    # TODO: better way to install using pip install
+                    subprocess.call("pip install --index-url http://bbpsrv19.epfl.ch:9090/simple/ --pre "+libs[i], shell=True)
+                else:
+                    # task-sdk is not installed
+                    pass
+            else:
+                if raw_input("you want to install it (Y/N)") is 'Y':
+                    # prompt you want to install it (Y/N)
+                    subprocess.call("pip install "+libs[i], shell=True)
+                else:
+                    pass
+else:
+    pass
+
+# import PyInstaller as py
+#
+# if py.is_linux or py.is_unix is True:
+#     print "OS Linux"
+# elif py.is_win is True:
+#     print "OS Win"
+# else:
+#     print "OS unknown"
+
+# Create binary-executable file at ./bin/
+
+# Save all meta input-infos to config.log or elephant.cfg
---
+++ start.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import argparse
+# import sys
+import json
+# import elephant as el
+import interface.elephant_main as ie
+
+# ----------------------------------
+#
+#       Command Line Interface
+#
+# ----------------------------------
+
+# def elephant_cli(args):
+#     pass
+#
+#
+type_spiketrain = set(['numpy', 'quantities', 'neo'])
+data_type_elephant = []
+
+
+def convert_type_float(t):
+    if t is '':
+        return None
+    else:
+        return float(t)
+
+
+def install_lib(package):
+    import pip
+    installed_listlib = sorted(["%s" % (i.key) for i in
+                                pip.get_installed_distributions()])
+    if package in installed_listlib:
+        return True
+    else:
+        return False
+
+
+def elephant_cli_conversion():
+    # Call main interface elephant
+    # print "Input Data for value of binarize_function"
+    # t_spiketrain = raw_input("choose one of following types for spiketrain\
+    #                        [numpy/ quantities/ neo] ")
+    t_spiketrain = "numpy"
+    if t_spiketrain in type_spiketrain:
+        pass
+    else:
+        raise ValueError
+
+    # value = raw_input("Value for spiketrain, list or list of list: ")
+    value = "[1.23, 0.3, 0.87, 0.56]"
+    # sampling_rate = convert_type_float(raw_input("Sampling_rate value\
+    #                                             (optional) default=None: "))
+    sampling_rate = 100
+    # t_start = convert_type_float(raw_input("t_start value (optional),\
+    #                                       default=None: "))
+    t_start = 0
+    # t_stop = convert_type_float(raw_input("t_stop value (optional),\
+    #                                      default=None: "))
+    t_stop = 10.0
+    return_times = raw_input("return_times bool-value (optional) True/ False ")
+
+    # TODO: block access to Elephant_sub ie.ElephantConv.conversion_binarize
+    result = ie.Elephant_main.start_elephant_conversion(t_spiketrain,
+                                                        json.loads(value),
+                                                        sampling_rate,
+                                                        t_start, t_stop,
+                                                        return_times)
+    print result
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Command Line Interface')
+    parser.add_argument("-s", "--show", action='store_true', help="List of all\
+                        functional interfaces")
+    parser.add_argument("-el", "--elephant", action='store_true',
+                        help="ElePhAnt interface")
+    parser.add_argument("-up", "--unifiedportal", action='store_true',
+                        help="Unified Portal interface")
+    args = parser.parse_args()
+
+    if args.show:
+        print "-------------- List of Built-in functions"
+        print "ElePhAnt: ...."
+        print "ElePhAnt: ...."
+        print "Unified Portal: get_task"
+        print "Unified Portal: get_all_tasks"
+        print "Unified Portal: get_job"
+        print "Unified Portal: get_all_jobs"
+    elif args.elephant:
+        print "-------------- ElePhAnt Interface"
+        print "First test: automatically calling function conversion ..."
+        elephant_cli_conversion()
+    elif args.unifiedportal:
+        if install_lib('task-sdk'):
+            print "Unified Portal interface"
+            tasks = ['get_task', 'get_all_tasks', 'get_job', 'get_all_jobs']
+            subcommand = raw_input('Input callable function:  ')
+            if subcommand in tasks:
+                from up.query_up import parse_args as pa
+                if subcommand == 'get_task':
+                    pa(subcommand)
+                elif subcommand in tasks and subcommand == 'get_all_tasks':
+                    pa(subcommand)
+                elif subcommand in tasks and subcommand == 'get_job':
+                    pa(subcommand)
+                else:
+                    print "call get_all_jobs func"
+                    pa("get_all_jobs")
+            else:
+                print"No suitable function found, 'start.py -s' to get help"
+        else:
+            print "You need to install library task-sdk to use U.P. functions"
+    else:
+        print "Input subcommand choices ['-s','-el','-up']"
+
+# Feature: Support analysis data from datapath as input ................
+    # if param.function == 'binarize':
+    #     elephant_cli_conversion()
+    # elif param.function == 'typedata':
+    #     # some code to test type of input data path
+    #     data_path = raw_input("Input data path to file ")
+    #     from neo.io import get_io
+    #     try:
+    #         x = get_io(data_path)
+    #         print "File Format %s is supported by neo", type(x)
+    #         print "All functions regarding are available to apply"
+    #         # from neo.io import iolist
+    #         # y = type(x) in iolist
+    #         # if y:
+    #         #     print type(x)
+    #         # else:
+    #         #    print "File-Format is not supported by neo"
+    #     except IOError:
+    #         print "IOError, File-Format is not supported by neo"
+
+    # else:
+    #     print "other functions are still being developed"
+
+    # raw_input (optional) values
+    # json.loads(value) has type list
